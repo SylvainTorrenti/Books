565c26669974771ee723f9f6a9aa325909be3da0
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-11-21 08:39:48 +0100

    Add project files.

---
+++ API/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.PatternContexts;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetBooks()
+        {
+            return Ok(Book.books);
+        }
+
+        [Route("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = Book.books.Find(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            book.Id = Book.Compteur;
+
+            Book.books.Add(book);
+
+            if (Book.books.Exists(b => book.Id == b.Id)) 
+            {
+               return CreatedAtAction(nameof(GetBookById), new { id = book.Id}, null);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+    }
+}
---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain.Entities
+{
+    public class Book
+    {
+        public static List<Book> books = new List<Book>() { 
+            new Book("TitleTest1","AuthorTest1","DespritionTest1"),
+            new Book("TitleTest2","AuthorTest2","DespritionTest2"),
+            new Book("TitleTest3","AuthorTest3","DespritionTest3"),
+            new Book("TitleTest4","AuthorTest4","DespritionTest4"),
+            new Book("TitleTest5","AuthorTest5","DespritionTest5"),
+        };
+
+        public static int Compteur { get; private set; } = 5;
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+
+        public Book()
+        {
+            Compteur = Compteur + 1;
+            Id = Compteur;
+        }
+        public Book(string? title, string? author, string? description)
+        {
+            Compteur = Compteur + 1;
+            Id = Compteur;
+            Title = title;
+            Author = author;
+            Description = description;
+        }
+        
+    }
+}
