2d00dedeeb8898a3cc1a2c98b8e747989170d280
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-11-21 10:26:19 +0100

    add crud

--- API/Controllers/BooksController.cs
+++ API/Controllers/BooksController.cs
@@ -8,14 +8,19 @@ namespace API.Controllers
     [Route("api/books")]
     public class BooksController : ControllerBase
     {
+        #region Get
+        #region Get All
         [HttpGet]
         [Route("")]
         public IActionResult GetBooks()
         {
             return Ok(Book.books);
         }
-
-        [Route("{id}")]
+        #endregion
+        #region Get by id
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetBookById(int id)
         {
             var book = Book.books.Find(b => b.Id == id);
@@ -26,26 +31,71 @@ namespace API.Controllers
             }
             return Ok(book);
         }
+        #endregion
+        #endregion
 
-        [HttpPost]
-        [Route("")]
+        #region Post
+        [HttpPost()]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult CreateBook([FromBody] Book book)
         {
             book.Id = Book.Compteur;
 
             Book.books.Add(book);
 
-            if (Book.books.Exists(b => book.Id == b.Id)) 
+            if (Book.books.Exists(b => book.Id == b.Id))
             {
-               return CreatedAtAction(nameof(GetBookById), new { id = book.Id}, null);
+                return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, null);
             }
             else
             {
                 return BadRequest();
             }
         }
+        #endregion
 
+        #region Put
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult ModifyBook([FromRoute] int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                Book bookModified = book;
+                if (bookModified != null)
+                {
+                    return Ok(bookModified);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+        #endregion
 
-
+        #region Delete
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]        public IActionResult DeleteBook([FromRoute] int id)
+        {
+            var book = Book.books.Find(b => b.Id == id);
+            if (id == book.Id)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        } 
+        #endregion
     }
 }
